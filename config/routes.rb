2d107f6624863b5e4034ab3fab0965e2d410ab42
Sergey Voronov <sergey.voronov@sibers.com>
2015-07-05 18:50:51 +0600

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -8,6 +9,10 @@ Rails.application.routes.draw do
   get 'signup'  => 'users#new'
 
   resources :users
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
